0fd41f9de732c47bb9f501caec0f921ba80b4cdc
Joanna092 <kochanowska92@gmail.com>
2021-09-26 23:11:12 +0200

    created simple view of calculator

--- src/App.js
+++ src/App.js
@@ -1,12 +1,41 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
 
 function App() {
   return (
-    <div className="box">
+    <div className="container border-black border-2">
+      <div className="row border-black border-2">
+        <span className="text-center">DISPLAY</span>
+      </div>
 
+      <div className="row">
+        <div className="col border-black border-2">1</div>
+        <div className="col border-black border-2">2</div>
+        <div className="col border-black border-2">3</div>
+        <div className="col border-black border-2">+</div>
+      </div>
+
+      <div className="row">
+        <div className="col border-black border-2">4</div>
+        <div className="col border-black border-2">5</div>
+        <div className="col border-black border-2">6</div>
+        <div className="col border-black border-2">-</div>
+      </div>
+
+      <div className="row">
+        <div className="col border-black border-2">7</div>
+        <div className="col border-black border-2">8</div>
+        <div className="col border-black border-2">9</div>
+        <div className="col border-black border-2">x</div>
+      </div>
+
+      <div className="row">
+        <div className="col border-black border-2">AC</div>
+        <div className="col border-black border-2">0</div>
+        <div className="col border-black border-2">=</div>
+        <div className="col border-black border-2">/</div>
+      </div>
     </div>
-  )
+  );
 }
 
 export default App;
